5a09b4a476eeac78e9a3991ff6227bfd4c450c92
hoc <hoc@F44M-HOCO-010.FORMATION.PDL>
2020-09-25 11:22:28 +0200

    ajout des methodes Bidding Buy_lot et Player_to_Player_Transaction à bank

--- classes/bank.py
+++ classes/bank.py
@@ -1,6 +1,9 @@
 from classes.player import Player
 
 class Bank:
+    def __init__ (self, Players):
+        self.Players = Players
+
     def Get_from_player(self):
         pass
 
@@ -10,13 +13,18 @@ class Bank:
     def Build_house(self):
         pass
 
-    def Bidding(self):
-        pass
+    def Bidding(self, Lot):
+        Bidders = Players
+        while len(Bidders) > 1:
+            for Bidder in Bidders:
+                if not Bidder.Bid() : Bidders.remove(Bidder)
+        Lot.Owner = Bidders[0]
 
-    def Buy_Lot(self):
-        pass
+    def Buy_Lot(self, Lot, Player):
+        Lot.Owner = Player
     
-    def Player_to_player_Transaction(self):
-        pass
+    def Player_to_player_Transaction(self, Player1, Player2, Money):
+        Player1.money -= Money
+        Player2.money += Money
     
  
--- tests/TBank.py
+++ tests/TBank.py
@@ -1,5 +1,10 @@
 from unittest import main
 from unittest import TestCase
+from unittest import assertTrue
+from classes.bank import Bank
+from classes.player import Player
+from classes.lot import Lot
+
 class TBank (TestCase):
     def TGet_from_player(self):
         pass
@@ -11,12 +16,25 @@ class TBank (TestCase):
         pass
 
     def TBidding(self):
+        # C = Card()
+        # Bank = Bank()
+        # Bank.bidding(C)
         pass
 
     def TBuy_Lot(self):
-        pass
+        L = Lot()
+        P = Player()
+        Bank = Bank()
+        Bank.Buy_Lot(L,P)
+        assertTrue(L.Owner == P)
 
     def TPlayer_to_player_Transaction(self):
-        pass
+        P1 = Player()
+        P2 = Player()
+        initMoneyP1 = P1.money
+        initMoneyP2 = P2.money
+        Bank = Bank()
+        Bank.Player_to_player_Transaction(P1,P2,100)
+        assertTrue(P1.money == initMoneyP1 - 100 and P2.money == initMoneyP2 + 100)
 
 main()
